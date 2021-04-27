2bb2d0e55d0adcf8b198a41778a0c865b75d25cc
fitzgebt <fitzgebt@gmail.com>
2021-04-27 16:49:30 -0700

    commit tree cfce272a90616606eeab0c4b49c6367f4ce662b3

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
 class Formatter {
   //add static methods here
+    static capitalize(string) {
+      return string.charAt(0).toUpperCase() + string.slice(1)
+    }
+
+    static sanitize(string) {
+      return string.replace( /[^A-Za-z0-9 '-]/g, '' )
+    }
+
+    static titleize (string) {
+      let nonCap = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+
+      let stringArr = string.split(' ')
+
+      let finalArr = []
+
+
+        for (let i = 0; i < stringArr.length; i++) {
+          if (i == 0) {
+            finalArr.push(this.capitalize(stringArr[i]))
+          } else { 
+            if (nonCap.includes(stringArr[i])) {
+              finalArr.push(stringArr[i])
+            } else {
+              finalArr.push(this.capitalize(stringArr[i]))
+            }
+          }
+        }
+        return finalArr.join(" ")
+  }
 }
